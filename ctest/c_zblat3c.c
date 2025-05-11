b167ae1baed73ddacddbad962ab294cc31683de6
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2025-05-11 18:29:33 +0200

    Update c_zblat3c.c

--- ctest/c_zblat3c.c
+++ ctest/c_zblat3c.c
@@ -2307,9 +2307,7 @@ L120:
 				    if (*trace) {
 					zprcn3_(ntra, &nc, sname, iorder, 
 						side, uplo, transa, diag, &m, 
-						&n, &alpha, &lda, &ldb, (
-						ftnlen)13, (ftnlen)1, (ftnlen)
-						1, (ftnlen)1, (ftnlen)1);
+						&n, &alpha, &lda, &ldb);
 				    }
 				    if (*rewi) {
 					al__1.aerr = 0;
@@ -2324,9 +2322,7 @@ L120:
 				    if (*trace) {
 					zprcn3_(ntra, &nc, sname, iorder, 
 						side, uplo, transa, diag, &m, 
-						&n, &alpha, &lda, &ldb, (
-						ftnlen)13, (ftnlen)1, (ftnlen)
-						1, (ftnlen)1, (ftnlen)1);
+						&n, &alpha, &lda, &ldb);
 				    }
 				    if (*rewi) {
 					al__1.aerr = 0;
