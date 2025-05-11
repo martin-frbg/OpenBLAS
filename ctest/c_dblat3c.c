11004a77d7f1eeb6e407068bdcdc4e61cf282404
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2025-05-11 18:27:31 +0200

    Update c_dblat3c.c

--- ctest/c_dblat3c.c
+++ ctest/c_dblat3c.c
@@ -2234,9 +2234,7 @@ L120:
 				    if (*trace) {
 					dprcn3_(ntra, &nc, sname, iorder, 
 						side, uplo, transa, diag, &m, 
-						&n, &alpha, &lda, &ldb, (
-						ftnlen)13, (ftnlen)1, (ftnlen)
-						1, (ftnlen)1, (ftnlen)1);
+						&n, &alpha, &lda, &ldb);
 				    }
 				    if (*rewi) {
 					al__1.aerr = 0;
@@ -2251,9 +2249,7 @@ L120:
 				    if (*trace) {
 					dprcn3_(ntra, &nc, sname, iorder, 
 						side, uplo, transa, diag, &m, 
-						&n, &alpha, &lda, &ldb, (
-						ftnlen)13, (ftnlen)1, (ftnlen)
-						1, (ftnlen)1, (ftnlen)1);
+						&n, &alpha, &lda, &ldb);
 				    }
 				    if (*rewi) {
 					al__1.aerr = 0;
